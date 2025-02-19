e3b5d84fb1e3c3a35085dbdf23623b8dbd79c891
Jkean2012 <Jkean2012@outlook.com>
2025-02-19 16:07:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://educationbluesky.com/';
+const nggUrl = 'https://mathstutor.life/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'https://educationbluesky.com/') {
+    if (req.headers.host === 'https://mathstutor.life/') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
